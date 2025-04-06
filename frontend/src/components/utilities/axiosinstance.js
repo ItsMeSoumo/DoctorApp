daa8aaa8d06a34a4cc58812758cf4c6a6596798e
SOUMO <soumo2020.saha@gmail.com>
2025-04-07 01:54:34 +0530

    fix: Add authentication token to axios requests

--- frontend/src/components/utilities/axiosinstance.js
+++ frontend/src/components/utilities/axiosinstance.js
@@ -9,3 +9,17 @@ export const axiosinstance = axios.create({
     "Content-Type": "application/json",  
   }, 
 });
+
+// Add request interceptor to include token
+axiosinstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
